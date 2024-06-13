3a4b8c6354f4ed7922763d3978f89bcbc1dfd6b1
Adriel Ardiles <adrielardiles132004@gmail.com>
2024-06-13 00:09:41 -0500

    First commit backend

---
+++ TimeTravelecommerce/src/main/java/com/titmetravelshop/timetravel/TimeTravelecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.titmetravelshop.timetravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTravelecommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeTravelecommerceApplication.class, args);
+    }
+
+}
---
+++ TimeTravelecommerce/src/test/java/com/titmetravelshop/timetravel/TimeTravelecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.titmetravelshop.timetravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTravelecommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
